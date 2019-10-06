5af675ccd5352d140d44a408cd81f28551c58b08
Bruno Paullo <brunopaullom@hotmail.com>
2019-10-06 14:06:12 -0500

    Javascript, Jquery, Google Maps

---
+++ Cap01/MiPrimeraWeb/MiPrimeraWeb/scripts/map.js
@@ -0,0 +1,21 @@
+﻿(function (cibertec) {
+    window.cibertec.geoLocation = function () {
+        console.log("Verificado Geolocalizacion");
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+            console.log("No se Admite Geolocalizacion");
+        }
+    }
+    function showPosition(position) {
+        var location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        }
+
+        var map = new google.maps.Map(document.getElementById("googleMap"), { zoom: 15, canter: location });
+
+        var marker = new google.maps.Marker({ position: location, map: map, title: "Cibertec" });
+    }
+})(window.cibertec = window.cibertec || {});
