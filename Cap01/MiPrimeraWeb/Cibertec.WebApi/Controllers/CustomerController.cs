18c9e2617a58dcad0c6113c20d01232ac9b15d6e
Bruno Paullo <brunopaullom@hotmail.com>
2019-12-29 14:18:00 -0500

    Init WebApi, Cap 09, 29/12/2019

---
+++ Cap01/MiPrimeraWeb/Cibertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Cibertec.Models;
+using Cibertec.UnitOfWork;
+
+namespace Cibertec.WebApi.Controllers
+{
+    [RoutePrefix("customer")]
+    public class CustomerController : BaseController
+    {
+        public CustomerController(IUnitOfWork unit) : base(unit)
+        {
+        }
+
+        [Route("{id}")]
+        public IHttpActionResult Get(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return BadRequest();
+            return Ok(_unit.Customers.GetById(id));
+        }
+
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult Post(Customers customer)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var id = _unit.Customers.Insert(customer);
+            return Ok(new { id = id });
+        }
+
+        [Route("")]
+        [HttpPut]
+        public IHttpActionResult Put(Customers customer)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!_unit.Customers.Update(customer)) return BadRequest(ModelState);
+            return Ok(new { status = true });
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id)) return BadRequest();
+            var result = _unit.Customers.Delete(id);
+            return Ok(new { delete = true });
+        }
+
+        [Route("list")]
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            return Ok(_unit.Customers.GetList());
+        }
+    }
+}
---
+++ Cap01/MiPrimeraWeb/Cibertec.WebApi/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Cibertec.WebApi.App_Start;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Cibertec.WebApi.Startup))]
+
+namespace Cibertec.WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            DIConfig.ConfigureInjector(config);
+            TokenConfig.ConfigureAuth(app, config);
+            RouteConfig.Register(config);
+            
+            app.UseWebApi(config);
+        }
+    }
+}
